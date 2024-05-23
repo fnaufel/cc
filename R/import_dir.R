6428bfd2d4420ec98ce668ca7965bbe84cfe0acb
Fernando Naufel <fnaufel@gmail.com>
2024-05-23 16:22:32 -0300

    Fix error in import_dir()
    One of df_matched and df_unmatched may be empty, so I test for that.

--- R/import_dir.R
+++ R/import_dir.R
@@ -7,7 +7,7 @@
 #' In both filenames above, `(...)` is the current time in the format `yy_mm_dd_hh_mm_ss`.
 #'
 #' @param dirname Directory as string.
-#' @param due_date Date of current fatura.
+#' @param due_date Date of current fatura. String format dd-mm-yyyy or dd-mm-yy
 #' @param categorize If `TRUE`, assign new categories to items.
 #' @param mapping Path and name of a csv file containing 2 columns: `item` and `category`. Used if `categorize == TRUE`.
 #' @author Fernando Naufel
@@ -64,6 +64,9 @@ import_dir <- function(
     )
 
   # Items that matched
+  # The way I detect this is not nice:
+  # if categoria has a vertical bar "|", it means categoria comes
+  # from the table; otherwise, it comes from fatura
   df_matched <- df %>%
     dplyr::filter(
       stringr::str_detect(
@@ -82,18 +85,22 @@ import_dir <- function(
     )
 
   # Write to files
-  df_matched %>%
-    write_df(
-      fs::path_norm(
-        fs::path_join(c(dirname, 'df_matched'))
+  if (nrow(df_matched) > 0) {
+    df_matched %>%
+      write_df(
+        fs::path_norm(
+          fs::path_join(c(dirname, 'df_matched'))
+        )
       )
-    )
+  }
 
-  df_unmatched %>%
-    write_df(
-      fs::path_norm(
-        fs::path_join(c(dirname, 'df_unmatched'))
+  if (nrow(df_unmatched) > 0) {
+    df_unmatched %>%
+      write_df(
+        fs::path_norm(
+          fs::path_join(c(dirname, 'df_unmatched'))
+        )
       )
-    )
+  }
 
 }
